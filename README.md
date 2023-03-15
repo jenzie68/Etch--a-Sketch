5238b5a896e39d2381c1be9b5abaa975e969a328
jenzie <jacecabalo@gmail.com>
2023-03-15 16:28:30 +0400

    reset code and make again

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # etch-a-Sketch
 etch a sketch project from TOP
 
-const myDiv = document.getElementById('myDiv');
-const containerEl = document.querySelector('.container');
-
-let numOfDivs = prompt('how many squares do you want to put?',10);
-containerEl.style.display = 'grid';
-containerEl.style.gridTemplateColumns = `repeat(${numOfDivs},50px)`;
-containerEl.style.gridTemplateRows = `repeat(${numOfDivs},50px)`;
--- script.js
+++ script.js
@@ -1,30 +1,70 @@
+let container = document.querySelector('.container');
 
-const containerEl = document.querySelector('.container');
+function makeGrid() {
+    let userInput = prompt('input size');
+    
+    container.style.gridTemplateColumns = `repeat(${userInput},1fr)`;
+    container.style.gridTemplateRows = `repeat(${userInput},1fr)`;
 
-let numOfDivs = prompt('how many squares do you want to put?',10);
-containerEl.style.display = 'grid';
-containerEl.style.gridTemplateColumns = `repeat(${numOfDivs},50px)`;
-containerEl.style.gridTemplateRows = `repeat(${numOfDivs},50px)`;
+    function makeSquares(num) {
 
-let createDivs = (num) => {
+        for (s = 0;s < num*num; s++) {
+            let div = document.createElement('div');
+            div.style.backgroundColor = '#d87093';
+            div.classList.add('square');
+            container.appendChild(div);
+        };
 
-    for (let i = 0; i < num*num; i++) {
-        let div = document.createElement("div");
-        div.classList.add('square');
-        div.style.width = '50px';
-        div.style.height = '50px';
-        div.style.background = '#BE68D8';
-        containerEl.appendChild(div);
+        a = document.querySelectorAll('.square');
+
+        for(i in a) {
+            a[i].onmouseover = function() {
+                this.setAttribute('style','background-color:yellow; border-style: double; border-color:pink');
+            };
+        };  
+    };
+
+    makeSquares(userInput);
+};
+
+function makeRandomClr () {
+
+    function getRandomColor() {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        this.style.backgroundColor = '#' + randomColor
     };
 
     a = document.querySelectorAll('.square');
-    for(i in a) {
-        a[i].onmouseover=function(){this.setAttribute('style','background-color:blue;');}
-    };
 
+    a.forEach(item => {
+        item.addEventListener('mouseover',getRandomColor);
+    });
+
+}
+
+function removeSquares() {
+    while(container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    };
+    makeGrid();
 };
 
-createDivs(numOfDivs);
+const btnNewGrid = document.getElementById('new-grid');
+btnNewGrid.addEventListener('click', removeSquares);
+
+const btnClrChange = document.getElementById('color-change');
+btnClrChange.addEventListener('click',makeRandomClr);
+
+makeGrid();
+
+
+
+
+
+
+
+
+
 
 
 
